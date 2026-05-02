f8cf77f7d668be039c362e6288e83f721c5cbd43
Mafyaa <user@users-MacBook-Air.local>
2026-05-03 04:23:49 +0800

    Initial commit: Boilerplate SCM NestJS

--- costumer/src/auth/service/auth.service.ts
+++ costumer/src/auth/service/auth.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  // Tambahkan metode atau logika yang diperlukan
+  
   validateUser(username: string, password: string): boolean {
-    // Contoh implementasi sederhana
+    
     return username === 'admin' && password === '12345';
   }
 }
